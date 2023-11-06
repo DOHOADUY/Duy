27218409466f399da0fb00292c8a89ee9bbf3e75
DoHoaDuy <dohoaduy0201@gmail.com>
2023-11-06 23:09:51 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    introTitle: 'Phương Linh à!',
+    introDesc: `Có những cái sai không thể sửa được
+    Chắp vá gượng ép chỉ càng làm sai thêm
+    Chỉ có cách là đường bao giờ sai nữa, hoặc phải bù lại bằng một việc đúng khác`,
+    btnIntro: 'OK',
+    title: 'Phương Linh đi thực tập vui chứ?',
+    desc: 'sau một khoảng thời gian healing thì cậu biết đấy, nó chả tốt chút nào cả!  ',
+    btnYes: 'Rất vui',
+    btnNo: 'Không vui',
+    question:'Điều cậu muốn nói với tớ:',
+    btnReply: 'Gửi cho tớ',
+    mess: 'Tớ biết mà. Chúc cậu có một kì thực tập thật nhiều kỷ niệm',
+    messDesc: '.',
+    btnAccept: 'Cảm ơn cậu nhiều nha',
+    messLink: 'https://www.facebook.com/Nkoccary.do/' 
+}
